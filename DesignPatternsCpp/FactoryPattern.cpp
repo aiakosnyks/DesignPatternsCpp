6c2d32075e9fef8002701ebc7736bdaff9abe8a3
Melike Sümeyye Soydan <aiakosnyks@outlook.com>
2024-03-16 02:53:19 +0300

    Add project files.

---
+++ DesignPatternsCpp/FactoryPattern.cpp
@@ -0,0 +1,71 @@
+//Abstract product class
+
+#include <stdlib.h>
+#include <iostream>
+class Shape {
+	public:
+		virtual void draw() = 0;
+		virtual ~Shape() {} // Virtual destructor for polymorphism,
+};
+
+//Concrete product class - CIRCLE
+
+class Circle : public Shape {
+public:
+	void draw() override {
+		std::cout << "Drawing a Square"
+			<< std::endl;
+	}
+};
+
+//Concrete product class - SQUARE
+
+class Square : public Shape {
+public:
+	void draw() override {
+		std::cout << "Drawing a Square" 
+			<< std::endl;
+	}
+};
+
+//Abstract creator class
+class ShapeFactory {
+	public:
+		virtual Shape* createShape() = 0;
+		virtual ~ShapeFactory() {} //Virtual destructor for polymorphism
+};
+
+//Concrete creator class - CIRCLEFACTORY
+class CircleFactory : public ShapeFactory {
+	public:
+		Shape* createShape() override {
+			return new Circle();
+		}
+};
+
+class SquareFactory : public ShapeFactory {
+	public:
+		Shape* createShape() override {
+			return new Square();
+		}
+};
+
+int Main() {
+	ShapeFactory* circleFactory = new CircleFactory();
+	ShapeFactory* squareFactory = new SquareFactory();
+
+	Shape* circle = circleFactory -> createShape(); 
+	Shape* square = squareFactory -> createShape(); 
+
+	circle -> draw(); //Output: Draawing a circle
+	square -> draw(); //Output: Draawing a square
+
+	delete circleFactory;
+	delete squareFactory;
+	delete circle;
+	delete square;
+
+	return 0;
+
+
+}
